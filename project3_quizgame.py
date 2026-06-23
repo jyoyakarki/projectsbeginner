6237d6c8f5f9fd0c44b7fa8d66259ea9af4dfcaf
jyoyakarki <kjyoya908@gmail.com>
2026-06-23 15:01:11 +0545

    project4

--- project3_quizgame.py
+++ project3_quizgame.py
@@ -37,4 +37,4 @@ else:
     print("Incorrect!")
 
 print("You got " + str(score) + "questions correct!")
-print("You got " + str((score/4)*100) + "%.")
+print("You got " + str((score/4)*100) + "%.") 
